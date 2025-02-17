b4d1a8c6b3c4b055ba310fc83b2c65d2ee4a7472
Shreyas Kulkarni <shreyasnk532@gmail.com>
2025-02-16 18:07:37 -0800

    Interpret Design Pattern Extended

--- Interpreter Design Pattern/src/Main.java
+++ Interpreter Design Pattern/src/Main.java
@@ -3,8 +3,13 @@ public class Main {
         Context context = new Context();
         context.put("a", 2);
         context.put("b", 4);
+        context.put("c", 8);
+        context.put("d", 16);
 
-        AbstractExpression expression = new MultiplyNonTerminalExpression(new NumberTerminalExpression("a"), new NumberTerminalExpression("b"));
-        System.out.println(expression.interpret(context));
+        AbstractExpression expression1 = new MultiplyNonTerminalExpression(new NumberTerminalExpression("a"), new NumberTerminalExpression("b"));
+        System.out.println(expression1.interpret(context));
+
+        AbstractExpression expression2 = new SumNonTerminalExpression(new MultiplyNonTerminalExpression(new NumberTerminalExpression("a"), new NumberTerminalExpression("b")), new MultiplyNonTerminalExpression(new NumberTerminalExpression("c"),  new NumberTerminalExpression("d")));
+        System.out.println(expression2.interpret(context));
     }
 }
---
+++ Interpreter Design Pattern/src/SumNonTerminalExpression.java
@@ -0,0 +1,14 @@
+public class SumNonTerminalExpression implements AbstractExpression{
+    AbstractExpression leftExpression;
+    AbstractExpression rightExpression;
+
+    SumNonTerminalExpression(AbstractExpression leftExpression, AbstractExpression rightExpression) {
+        this.leftExpression = leftExpression;
+        this.rightExpression = rightExpression;
+    }
+
+    @Override
+    public int interpret(Context context) {
+        return leftExpression.interpret(context) + rightExpression.interpret(context);
+    }
+}
